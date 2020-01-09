5fa1acc42c980af0bd22693a28a5f68c516c6bbc
akira <40537272+AI1411@users.noreply.github.com>
2020-01-10 00:18:30 +0900

    Update firebase.js

--- nuxt-go-client/plugins/firebase.js
+++ nuxt-go-client/plugins/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth"
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    apiKey: "AIzaSyC507QuHKWoikVoPiVHeyWuyq_fd1IqwgA",
-    authDomain: "go-nuxt-c505212.firebaseapp.com",
-    databaseURL: "https://go-nuxt-c505212.firebaseio.com",
-    projectId: "go-nuxt-c505212",
-    storageBucket: "go-nuxt-c505212.appspot.com",
-    messagingSenderId: "431856509725",
-    appId: "1:431856509725:web:a8616fedd8af3108deb7f4"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   })
 }
 
