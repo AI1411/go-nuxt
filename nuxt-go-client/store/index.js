83af4acef9e73e8429b0742f6c761ad1c8fa47a7
AI1411 <goethe0719@gmail.com>
2020-01-09 23:48:28 +0900

    refreshToken追加

--- nuxt-go-client/store/index.js
+++ nuxt-go-client/store/index.js
@@ -1,77 +1,82 @@
-import {createRequestClient} from "./request-client";
+import {createRequestClient} from '~/store/request-client';
 import firebase from "~/plugins/firebase";
 
 export const state = () => ({
   items: [],
   relatedItems: [],
+  searchItems: [],
+  favoriteItems: [],
   item: {},
   meta: {},
-  favoriteItems: [],
-  searchItems: [],
   searchMeta: {},
-  token: ''
-});
+  token: '',
+})
 
 export const actions = {
   async fetchPopularVideos({commit}, payload) {
-    const client = createRequestClient(this.$axios);
-    const res = await client.get(payload.uri, payload.params);
-    commit('mutatePopularVideos', res);
+    const client = createRequestClient(this.$axios, this.$cookies, this)
+    const res = await client.get(payload.uri, payload.params)
+    commit('mutatePopularVideos', res)
   },
   async findVideo({commit}, payload) {
-    const client = createRequestClient(this.$axios);
-    const res = await client.get(payload.uri);
+    const client = createRequestClient(this.$axios, this.$cookies, this)
+    const res = await client.get(payload.uri)
     const params = {
       ...res.video_list,
-    };
-    params.isFavorite = res.is_favorite || false;
-    commit('mutateVideo', params);
+    }
+    params.isFavorite = res.is_favorite || false
+    commit('mutateVideo', params)
   },
   async fetchRelatedVideos({commit}, payload) {
-    const client = createRequestClient(this.$axios);
-    const res = await client.get(payload.uri);
-    commit('mutateRelatedVideos', res);
+    const client = createRequestClient(this.$axios, this.$cookies, this)
+    const res = await client.get(payload.uri)
+    commit('mutateRelatedVideos', res)
   },
   async searchVideos({commit}, payload) {
-    const client = createRequestClient(this.$axios)
+    const client = createRequestClient(this.$axios, this.$cookies, this)
     const res = await client.get(payload.uri, payload.params)
     commit('mutateSearchVideos', res)
   },
   async signUp({commit, dispatch}, payload) {
-    await firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password);
-    const res = await firebase.auth().signInWithEmailAndPassword(payload.email, payload.password);
-    const token = await res.user.getIdToken();
-    this.$cookies.set('jwt_token', token);
-    commit('mutateToken', token);
-    this.app.router.push('/');
+    await firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
+    const res = await firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+    const token = await res.user.getIdToken()
+    this.$cookies.set('jwt_token', payload)
+    const refreshToken = res.user.refreshToken
+    this.$cookies.set('refresh_token', refreshToken)
+    commit('mutateToken', token)
+    this.app.router.push('/')
   },
   async setToken({commit}, payload) {
-    commit('mutateToken', payload);
+    this.$cookies.set('jwt_token', payload)
+    commit('mutateToken', payload)
   },
   async login({commit, dispatch}, payload) {
-    const res = await firebase.auth().signInWithEmailAndPassword(payload.email, payload.password);
-    const token = await res.user.getIdToken();
-    this.$cookies.set('jwt_token', token);
-    commit('mutateToken', token);
-    this.app.router.push('/');
+    const res = await firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+    const token = await res.user.getIdToken()
+    this.$cookies.set('jwt_token', token)
+    const refreshToken = res.user.refreshToken
+    this.$cookies.set('refresh_token', refreshToken)
+    commit('mutateToken', token)
+    this.app.router.push('/')
   },
   async logout({commit}) {
-    await firebase.auth().signOut();
-    commit('mutateToken', null);
-    this.$cookies.remove('jwt_token');
-    this.app.router.push('/login')
+    await firebase.auth().signOut()
+    commit('mutateToken', null)
+    this.$cookies.remove('jwt_token')
+    this.app.router.push('/')
   },
   async toggleFavorite({commit}, payload) {
-    const client = createRequestClient(this.$axios);
-    const res = await client.post(payload.uri);
+    const client = createRequestClient(this.$axios, this.$cookies, this)
+    const res = await client.post(payload.uri)
     commit('mutateToggleFavorite', res.is_favorite)
   },
   async fetchFavoriteVideos({commit}, payload) {
-    const client = createRequestClient(this.$axios);
-    const res = await client.get(payload.uri);
-    commit('mutateFavoriteVideos', res);
+    const client = createRequestClient(this.$axios, this.$cookies, this)
+    const res = await client.get(payload.uri)
+    commit('mutateFavoriteVideos', res)
   }
-};
+}
 
 export const mutations = {
   mutatePopularVideos(state, payload) {
@@ -79,9 +84,9 @@ export const mutations = {
     state.meta = payload
   },
   mutateVideo(state, payload) {
-    const params = (payload.items && payload.items.length > 0) ? payload.items[0] : {};
-    params.isFavorite = payload.isFavorite || false;
-    state.item = params;
+    const params = (payload.items && payload.items.length > 0) ? payload.items[0] : {}
+    params.isFavorite = payload.isFavorite || false
+    state.item = params
   },
   mutateRelatedVideos(state, payload) {
     state.relatedItems = payload.items || []
@@ -90,16 +95,17 @@ export const mutations = {
     state.searchItems = payload.items ? state.searchItems.concat(payload.items) : []
     state.searchMeta = payload
   },
+  mutateFavoriteVideos(state, payload) {
+    state.favoriteItems = payload.items || []
+  },
+
   mutateToken(state, payload) {
-    state.token = payload;
+    state.token = payload
   },
   mutateToggleFavorite(state, payload) {
     state.item.isFavorite = payload
   },
-  mutateFavoriteVideos(state, payload) {
-    state.favoriteItems = payload.items || []
-  }
-};
+}
 
 export const getters = {
   getPopularVideos(state) {
@@ -126,4 +132,4 @@ export const getters = {
   isLoggedIn(state) {
     return !!state.token
   },
-};
+}
